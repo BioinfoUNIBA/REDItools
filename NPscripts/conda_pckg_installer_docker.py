d7173240ddf22d953f2e3046bb1b50486d9aad88
Claudio Lo Giudice <51158041+claudiologiudice@users.noreply.github.com>
2019-08-08 16:51:40 +0200

    Update conda_pckg_installer_docker.py

--- NPscripts/conda_pckg_installer_docker.py
+++ NPscripts/conda_pckg_installer_docker.py
@@ -41,12 +41,12 @@ while os.system(conda_url) != 0 and i <= 5:
 	os.system(conda_url)
 	i+=1
 os.system('chmod +x Miniconda2-latest-Linux-x86_64.sh')
-os.system('bash Miniconda2-latest-Linux-x86_64.sh')
+os.system('bash Miniconda2-latest-Linux-x86_64.sh -b')
 home_folder = os.path.expanduser('~')
 cmd = home_folder + '/miniconda2/bin/conda'
 os.system(cmd + ' config --add channels defaults')
 os.system(cmd + ' config --add channels bioconda')
 os.system(cmd + ' config --add channels conda-forge')
-os.system(cmd + ' create -n nature_protocol python=2.7 anaconda')
+os.system(cmd + ' create -n nature_protocol python=2.7 anaconda -y')
 install_conda_packages(cmd)
 print("Your conda environment has been succesfully created.") 
